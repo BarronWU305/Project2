75a4c890d91fe6a8866b3b3def0421d8d4b73832
BarronWU305 <barrona5@winthrop.edu>
2023-10-24 22:20:30 -0400

    added vars, comments, and got the file name

--- BankAccount.cc
+++ BankAccount.cc
@@ -1,12 +1,31 @@
 #include <iostream>
 #include <string>
+#include <fstream>
 #include <BankAccountRecords.h>
 
 using namespace std;
 
 int main()
 {
+    string firstname; 
+    string lastname; 
+    double amount; 
+    int AccountNums; 
+    const int MaxNumAccounts = 100; 
+    string filename; 
+    int countAccounts; 
 
-    // everything good
+    //GET THE FILE NAME OF THE FILE WE WILL BE READING FROM
+    cout << "Enter in the name of the file that we will be reading from below:" << endl; 
+    cin >> filename; 
+
+    ifstream inputFile(filename); 
+    if (!inputFile) {
+        cout << "ERROR: The file cannot open. Try again." << endl; 
+        return 1; //NOT IS ALL WELL!!
+    }
+
+    
+    //ALL WELL :D
     return 0;
 }
