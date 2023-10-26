f9e9874974c6b9a2c07a83f6041698ab4921f503
BarronWU305 <barrona5@winthrop.edu>
2023-10-26 19:11:04 -0400

    Finished Accessor and Mutator methods

--- BankRecords.cc
+++ BankRecords.cc
@@ -1,16 +1,54 @@
-#include<BankAccountRecords.h>
+#include <BankAccountRecords.h>
 
-//Initilization constructors for the bank record
+// Initilization constructors for the bank record
 
-BankAccountRecord::BankAccountRecord(string inFirstName, string inLastName, double inAmount, int inAccountNum) {
+BankAccountRecord::BankAccountRecord(string inFirstName, string inLastName, double inAmount, int inAccountNum)
+{
     FirstName = inFirstName;
-    LastName = inLastName; 
+    LastName = inLastName;
     Amount = inAmount;
     AccountNum = inAccountNum;
-
 }
 
-//Accessor and mutator for first name
-string BankAccountRecord::GetFirstName() const {
-    return FirstName; 
+// Accessor and mutator for first name
+string BankAccountRecord::GetFirstName() const
+{
+    return FirstName;
+}
+
+void BankAccountRecord::SetFirstName(string inFirstName)
+{
+    FirstName = inFirstName;
+}
+
+// Accessor and mutator for the last name
+string BankAccountRecord::GetLastName() const
+{
+    return LastName;
+}
+
+void BankAccountRecord::SetFirstName(string inLastName)
+{
+    LastName = inLastName;
+}
+
+// Accessor and mutator for Amount
+double BankAccountRecord::GetAmount() const
+{
+    return Amount;
+}
+void BankAccountRecord::SetAmount(double inAmount)
+{
+    Amount = inAmount;
+}
+
+// Accessor and mutator for Account number
+int BankAccountRecord::GetAccountNum() const
+{
+    return AccountNum;
+}
+
+void BankAccountRecord::SetAccountNum(double inAccountNum)
+{
+    AccountNum = inAccountNum;
 }
