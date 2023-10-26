71cc5f301abdd4e6cd27b9cc89d6bd1f56447bd0
BarronWU305 <barrona5@winthrop.edu>
2023-10-26 19:44:30 -0400

    switched around the last and first

--- BankRecords.cc
+++ BankRecords.cc
@@ -2,8 +2,7 @@
 
 // Initilization constructors for the bank record
 
-BankAccountRecord::BankAccountRecord(string inFirstName, string inLastName, double inAmount, int inAccountNum)
-{
+BankAccountRecord::BankAccountRecord(string inFirstName, string inLastName, double inAmount, int inAccountNum) {
     FirstName = inFirstName;
     LastName = inLastName;
     Amount = inAmount;
@@ -11,49 +10,41 @@ BankAccountRecord::BankAccountRecord(string inFirstName, string inLastName, doub
 }
 
 // Accessor and mutator for first name
-string BankAccountRecord::GetFirstName() const
-{
+string BankAccountRecord::GetFirstName() const {
     return FirstName;
 }
 
-void BankAccountRecord::SetFirstName(string inFirstName)
-{
+void BankAccountRecord::SetFirstName(string inFirstName) {
     FirstName = inFirstName;
 }
 
 // Accessor and mutator for the last name
-string BankAccountRecord::GetLastName() const
-{
+string BankAccountRecord::GetLastName() const {
     return LastName;
 }
 
-void BankAccountRecord::SetFirstName(string inLastName)
-{
+void BankAccountRecord::SetFirstName(string inLastName) {
     LastName = inLastName;
 }
 
 // Accessor and mutator for Amount
-double BankAccountRecord::GetAmount() const
-{
+double BankAccountRecord::GetAmount() const {
     return Amount;
 }
-void BankAccountRecord::SetAmount(double inAmount)
-{
+void BankAccountRecord::SetAmount(double inAmount) {
     Amount = inAmount;
 }
 
 // Accessor and mutator for Account number
-int BankAccountRecord::GetAccountNum() const
-{
+int BankAccountRecord::GetAccountNum() const {
     return AccountNum;
 }
 
-void BankAccountRecord::SetAccountNum(double inAccountNum)
-{
+void BankAccountRecord::SetAccountNum(double inAccountNum) {
     AccountNum = inAccountNum;
 }
 
 //Print function 
 void BankAccountRecord::Print() const {
-    cout << " " << setw(25) << left << GetFirstName() << setw(15) << GetLastName() << setw(15) << right << "$" << GetAmount() << setw(15) << right << GetAccountNum << endl; 
+    cout << " " << setw(25) << left << GetLastName() << setw(15) << GetFirstName() << setw(15) << right << "$" << GetAmount() << setw(15) << right << GetAccountNum << endl; 
 }
