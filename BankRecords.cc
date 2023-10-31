ae62d437e75afa9a474376938752c2e8cc466144
BarronWU305 <barrona5@winthrop.edu>
2023-10-30 22:02:14 -0400

    setw

--- BankRecords.cc
+++ BankRecords.cc
@@ -57,6 +57,6 @@ void BankAccountRecord::SetAccountNum(double inAccountNum)
 void BankAccountRecord::printInfo() const
 {
     cout << "--------------------------------------------------------------------------" << endl;
-    cout << setw(15) << GetFirstName() << " " << GetLastName() << " (" << GetAccountNum() << "): ";
-    cout << fixed << setw(15) << setprecision(2) << "$" << GetAmount() << endl;
+    cout << setw(0) << GetFirstName() << " " << GetLastName() << " (" << GetAccountNum() << "): ";
+    cout << fixed << setw(2) << setprecision(2) << "$" << GetAmount() << endl;
 }
